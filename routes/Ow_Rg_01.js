5db3745c83b40b6a63b37dd7d56c8f74f6e90977
jihye <wisdom467@naver.com>
2016-08-05 12:47:24 +0900

    alert & 입력 안했을 경우 경고

--- routes/Ow_Rg_01.js
+++ routes/Ow_Rg_01.js
@@ -1,10 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+
+
+var connection = mysql.createConnection({
+	host : '14.63.196.48' ,
+	port : '3306',
+	user : 'root',
+	password : 'Rkakdqpfm!00',
+	database : 'oshow'
+});
+
 /* GET Page. */
 router.get('/', function(req, res, next) {
 res.render('Ow_Rg_01');
 
 });
+
+
+router.post('/', function(req,res){
+	var restaurant = {
+			'restaurant_name':req.body.restaurant_name,
+            'restaurant_opening_time':req.body.restaurant_opening_time,
+            'restaurant_closing_time':req.body.restaurant_closing_time,
+            'restaurant_type':req.body.restaurant_type,
+            'restaurant_address':req.body.restaurant_address,
+            'restaurant_tel':req.body.restaurant_tel
+            };
+	var query = connection.query('insert into restaurant set ?',restaurant,function(err,result){
+    if (err) {
+        console.error(err);
+        throw err;
+    }
+    else {
+    	res.redirect('/Ow_Rg_01');
+   	   //res.send("<script>alert('저장되었습니다.');</script>");
+    	
+    }
+    
+     console.log(query);
+   
+});
+	
+});
+
+
 
 module.exports = router;
--- routes/Ow_Rg_01_Success.js
+++ routes/Ow_Rg_01_Success.js
@@ -18,16 +18,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
+function restaurant_regist() {
+	 var restaurant = {
+ 			'restaurant_name':req.body.restaurant_name,
+             'restaurant_opening_time':req.body.restaurant_opening_time,
+             'restaurant_closing_time':req.body.restaurant_closing_time,
+             'restaurant_type':req.body.restaurant_type,
+             'restaurant_address':req.body.restaurant_address,
+             'restaurant_tel':req.body.restaurant_tel
+             };
+	 console.log(restaurant);
+}
 //insert
 router.post('/',function(req,res){
     var restaurant = {
-    			'restaurant_name':req.body.restaurant_name,
-                'restaurant_opening_time':req.body.restaurant_opening_time,
-                'restaurant_closing_time':req.body.restaurant_closing_time,
-                'restaurant_type':req.body.restaurant_type,
-                'restaurant_address':req.body.restaurant_address,
-                'restaurant_tel':req.body.restaurant_tel
+    			'restaurant_name': $("#R_INFO .restaurant_name").val(),
+                'restaurant_opening_time': $("#R_INFO .restaurant_opening_time").val(),
+                'restaurant_closing_time': $("#R_INFO .restaurant_closing_time").val(),
+                'restaurant_type':$("#R_INFO .restaurant_closing_time").val(),
+                'restaurant_address': $("#R_INFO .restaurant_type").val(),
+                'restaurant_tel':$("#R_INFO .restaurant_tel").val()
                 };
     var query = connection.query('insert into restaurant set ?',restaurant,function(err,result){
         if (err) {
