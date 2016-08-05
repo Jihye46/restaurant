db3ffee3d194f5268f9967d3bfcfbe0b161f8eb2
jihye <wisdom467@naver.com>
2016-08-06 02:56:10 +0900

    페이지 접속 시 기존 정보 나타남 + 메뉴추가 버튼 필요

--- routes/Ow_Rg_01.js
+++ routes/Ow_Rg_01.js
@@ -11,22 +11,18 @@ var connection = mysql.createConnection({
 	database : 'oshow'
 });
 
-/* GET Page. */
-router.get('/', function(req, res, next) {
-res.render('Ow_Rg_01');
 
-});
 
 
 router.post('/', function(req,res){
 	var restaurant = {
 			'restaurant_name':req.body.restaurant_name,
-            'restaurant_opening_time':req.body.restaurant_opening_time,
-            'restaurant_closing_time':req.body.restaurant_closing_time,
-            'restaurant_type':req.body.restaurant_type,
-            'restaurant_address':req.body.restaurant_address,
-            'restaurant_tel':req.body.restaurant_tel
-            };
+	        'restaurant_opening_time':req.body.restaurant_opening_time,
+	        'restaurant_closing_time':req.body.restaurant_closing_time,
+	        'restaurant_type':req.body.restaurant_type,
+	        'restaurant_address':req.body.restaurant_address,
+	        'restaurant_tel':req.body.restaurant_tel
+	        };
 	var query = connection.query('insert into restaurant set ?',restaurant,function(err,result){
     if (err) {
         console.error(err);
@@ -46,7 +42,8 @@ router.post('/', function(req,res){
 
 router.post('/menu', function(req,res){
 	var menu = {
-			'restaurant_no':1,
+			//restaurant_no에 현재 owner가 등록한 레스토랑 넘버 가져와야 함
+			'restaurant_no':2,
 			'menu_name':req.body.menu,
             'price':req.body.price
             };
@@ -66,5 +63,34 @@ router.post('/menu', function(req,res){
 });
 	
 });
+
+
+router.get('/',function(req,res,next)
+		{	
+			
+			// 앞으로 datas에는  owner가 레스토랑 등록했을 때 부여 된 restaurant_no값 들어가야 함  
+			var datas = '1';
+			var query = connection.query('select restaurant_name,restaurant_opening_time,restaurant_closing_time,restaurant_type,restaurant_address,restaurant_tel from restaurant where restaurant_no=?' ,datas, function(err,row){
+				var menu_query = connection.query('select menu_name,price from menu where restaurant_no=?' , datas, function(err_menu,row_menu){
+				//레스토랑 정보 입력한 적이 없는 owner인 경우
+				//row가 null이면 메뉴도 등록이 될 수 없으므로 null
+				if(row =='')
+					{
+					 res.render('Ow_Rg_01', {title:"OSHOW 레스토랑 정보등록", row:"", row_menu:""});
+					}
+			    else {
+			    	console.log("레스토랑 정보 조회 결과 확인 : ",row);
+			    	console.log("메뉴 정보 조회 결과 확인 : ",row_menu);
+		            res.render('Ow_Rg_01', {title:"OSHOW 레스토랑 정보등록", row:row[0], row_menu:row_menu[0]});
+			    	
+			    }
+			    
+			     console.log(query);
+			     console.log(menu_query);
+			});
+		});	
+	});
+		    
+	
 
 module.exports = router;
